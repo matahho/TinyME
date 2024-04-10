3f95283a3e09e9ada61801a86d01ea9f36ffc1d9
notarealone <alireza.hosseini.81@gmail.com>
2024-04-10 11:55:26 +0330

    Added rollback for when order fails because of MEQ

--- src/main/java/ir/ramtung/tinyme/domain/service/Matcher.java
+++ src/main/java/ir/ramtung/tinyme/domain/service/Matcher.java
@@ -66,6 +66,11 @@ public class Matcher {
         if (result.outcome() == MatchingOutcome.NOT_ENOUGH_CREDIT)
             return result;
 
+        if (result.outcome() == MatchingOutcome.NOT_ENOUGH_INITIAL_EXECUTION){
+            rollbackTrades(order, result.trades());
+            return result;
+        }
+
         if (result.remainder().getQuantity() > 0) {
             if (order.getSide() == Side.BUY) {
                 if (!order.getBroker().hasEnoughCredit(order.getValue())) {
