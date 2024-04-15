3d0e33de160f36f61959087a32c95c5e7956c95a
matahho <mata1381@gmail.com>
2024-04-15 17:39:57 +0330

    add Stop-limit-order entity Model

---
+++ src/main/java/ir/ramtung/tinyme/domain/entity/StopLimitOrder.java
@@ -0,0 +1,39 @@
+package ir.ramtung.tinyme.domain.entity;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@Getter
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class StopLimitOrder extends Order{
+    int stopPrice;
+
+    public StopLimitOrder(long orderId, Security security, Side side, int quantity, int price, Broker broker, Shareholder shareholder, LocalDateTime entryTime, OrderStatus status, int stopPrice){
+        super(orderId, security, side, quantity, price, broker, shareholder, entryTime, status);
+        this.stopPrice = stopPrice;
+    }
+
+    public StopLimitOrder(long orderId, Security security, Side side, int quantity, int price, Broker broker, Shareholder shareholder, LocalDateTime entryTime, int stopPrice){
+        super(orderId, security, side, quantity, price, broker, shareholder, entryTime, status);
+        this.stopPrice = stopPrice;
+        this.status = OrderStatus.INACTIVE;
+
+    }
+
+    @Override
+    public Order snapshot(){
+        return new StopLimitOrder(orderId, security, side, quantity, price, broker, shareholder, entryTime, status, stopPrice);
+    }
+
+    @Override
+    public Order snapshotWithQuantity(int newQuantity){
+        return new StopLimitOrder(orderId, security, side, newQuantity, price, broker, shareholder, entryTime, status, stopPrice);
+    }
+
+
+
+}
