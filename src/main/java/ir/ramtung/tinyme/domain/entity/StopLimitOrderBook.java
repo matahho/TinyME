99d97662cf8538ccf928622b09829d6f30fe663d
matahho <mata1381@gmail.com>
2024-04-18 15:54:30 +0330

    add StopLimit Order Book entity

---
+++ src/main/java/ir/ramtung/tinyme/domain/entity/StopLimitOrderBook.java
@@ -0,0 +1,39 @@
+package ir.ramtung.tinyme.domain.entity;
+
+import java.util.LinkedList;
+
+public class StopLimitOrderBook extends OrderBook{
+
+    public LinkedList<Order> activatedStopLimits(int marketPrice){
+        LinkedList<Order> activated = new LinkedList<>();
+        activated.addAll(ActivatePossibleStopLimitsInBuyQueue(marketPrice));
+        activated.addAll(ActivatePossibleStopLimitsInSellQueue(marketPrice));
+        return activated;
+    }
+
+    private LinkedList<Order> ActivatePossibleStopLimitsInBuyQueue(int marketPrice) {
+        LinkedList<Order> activatedStopLimits = new LinkedList<>();
+        LinkedList<Order> buyQueue = getBuyQueue();
+        for (Order order : buyQueue) {
+            StopLimitOrder stopLimitOrder = ((StopLimitOrder) order); // Cast Type
+            if (stopLimitOrder.getStopPrice() >= marketPrice) {
+                Order activedOrder = stopLimitOrder.activate();
+                activatedStopLimits.push(activedOrder);
+            }
+        }
+        return activatedStopLimits;
+    }
+    private LinkedList<Order> ActivatePossibleStopLimitsInSellQueue(int marketPrice) {
+        LinkedList<Order> activatedStopLimits = new LinkedList<>();
+        LinkedList<Order> sellQueue = getSellQueue();
+        for (Order order : sellQueue) {
+            StopLimitOrder stopLimitOrder = ((StopLimitOrder) order); // Cast Type
+            if (stopLimitOrder.getStopPrice() <= marketPrice) {
+                Order activedOrder = stopLimitOrder.activate();
+                activatedStopLimits.push(activedOrder);
+            }
+        }
+        return activatedStopLimits;
+    }
+
+}
